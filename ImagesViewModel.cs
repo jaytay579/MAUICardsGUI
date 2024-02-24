9666a96b7a5cfa4360e59c99d0a66e8babf3463e
jaytay579 <jasontaylor579@gmail.com>
2024-02-24 02:29:22 +0000

    Add project files.

---
+++ ImagesViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace MAUICardsGUI
+{
+    public class ImagesViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<ImageItem> Images { get; set; }
+
+        public ImagesViewModel()
+        {
+            Images = new ObservableCollection<ImageItem> {
+                new ImageItem ("card_clubs_02.png", "2C"),
+                new ImageItem ("card_clubs_03.png", "3C"),
+                // etc.
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MAUICardsGUI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+
+}
